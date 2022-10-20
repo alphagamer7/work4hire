5cad1c49de8f63f36bbfbc2523a14b07703d22c8
preetyabhishek <theshivamsharma01@gmail.com>
2022-10-20 13:13:22 -0400

    Added user model

---
+++ work4hire/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace work4hire.Model
+{
+	public class User
+	{
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Image { get; set; }
+
+        public User(string name, string origin, string image)
+        {
+            Name = name;
+            Address = Address;
+            Image = image;
+        }
+
+	}
+}
+
