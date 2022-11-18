778779e4994077b259fb3de7f04e2b71deb951d2
shubhamdhamane <A00257743@mycambrian.ca>
2022-11-18 03:38:11 -0500

    action implemented for edit button
    after clicking edit button user can edit personal details and after clicking on save use won't be able to edit it. Basic idea is after clicking on save it will be save in firebase.

--- work4hire/Views/ProfilePage.xaml.cs
+++ work4hire/Views/ProfilePage.xaml.cs
@@ -2,12 +2,41 @@
 
 public partial class ProfilePage : ContentPage
 {
+    int count = 0;
 	public ProfilePage()
 	{
 		InitializeComponent();
 	}
 
+    void editDetails(System.Object sender, System.EventArgs e)
+    {
 
+
+        if (count == 0) {
+    
+            firstName.IsEnabled = true;
+            lastName.IsEnabled = true;
+            contactNo.IsEnabled = true;
+            email.IsEnabled = true;
+
+            edit.Text = "Save";
+            count = 1;
+        }
+        else
+        {
+            firstName.IsEnabled = false;
+            lastName.IsEnabled = false;
+            contactNo.IsEnabled = false;
+            email.IsEnabled = false;
+
+            edit.Text = "Edit";
+
+            count = 0;
+        }
+
+        
+
+    }
 
 
 }
