d99dafa72e23d7361f955f195437b0ade4a4f788
shubhamdhamane <A00257743@mycambrian.ca>
2022-11-30 23:27:20 -0500

    ProfilePic change option has been added
    In this I added read from file permissions for android. I used FilePicker to get a file from storage. I also set filetype to images so different image won't be selected.

--- work4hire/Views/ProfilePage.xaml.cs
+++ work4hire/Views/ProfilePage.xaml.cs
@@ -13,29 +13,50 @@ public partial class ProfilePage : ContentPage
     {
 
 
-        if (count == 0) {
+
+        //if (count == 0) {
     
-            firstName.IsEnabled = true;
-            lastName.IsEnabled = true;
-            contactNo.IsEnabled = true;
-            email.IsEnabled = true;
+        //    firstName.IsEnabled = true;
+        //    lastName.IsEnabled = true;
+        //    contactNo.IsEnabled = true;
+        //    email.IsEnabled = true;
 
-            edit.Text = "Save";
-            count = 1;
-        }
-        else
-        {
-            firstName.IsEnabled = false;
-            lastName.IsEnabled = false;
-            contactNo.IsEnabled = false;
-            email.IsEnabled = false;
+        //    edit.Text = "Save";
+        //    count = 1;
+        //}
+        //else
+        //{
+        //    firstName.IsEnabled = false;
+        //    lastName.IsEnabled = false;
+        //    contactNo.IsEnabled = false;
+        //    email.IsEnabled = false;
 
-            edit.Text = "Edit";
+        //    edit.Text = "Edit";
 
-            count = 0;
-        }
+        //    count = 0;
+        //}
 
         
+
+    }
+
+    async void editProfilePic(System.Object sender, System.EventArgs e)
+    {
+        var result = await FilePicker.PickAsync(new PickOptions
+        {
+            PickerTitle = "Pick Image please",
+            FileTypes = FilePickerFileType.Images
+
+        }) ;
+
+        if (result == null)
+        {
+            
+            return;
+        }
+
+        var stream = await result.OpenReadAsync();
+        ProfilePic.Source = ImageSource.FromStream(() => stream);
 
     }
 
