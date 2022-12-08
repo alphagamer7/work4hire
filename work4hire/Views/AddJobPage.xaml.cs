9cc005440073c17cfb07cd22d28577bea99a39b0
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-07 22:31:55 -0500

    Added check for internet connectivity, show error if no interent.

--- work4hire/Views/AddJobPage.xaml.cs
+++ work4hire/Views/AddJobPage.xaml.cs
@@ -15,11 +15,12 @@ public partial class AddJobPage : ContentPage
     AddJobsViewModel viewModel;
 
     public AddJobPage(AddJobsViewModel viewModel)
-    {
+    {   // start the defined components
         InitializeComponent();
         this.viewModel = viewModel;
         this.BindingContext = viewModel;
-        handleGeolocation();
+        handleGeolocation(); // Geolocation manages the user's location
+
     }
 
     public void handleGeolocation()
@@ -27,17 +28,17 @@ public partial class AddJobPage : ContentPage
         _ = this.viewModel.GetCurrentLocation();
     }
 
-    async void handleNavigation()
+    async void handleNavigation() // NavigationService provides the Navigation functionality
     {
         await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
     }
 
-    // TODO: refactor code
+    // 
     async void AddJobImage(Object sender,EventArgs e)
     {
         string action = await DisplayActionSheet("Open With", "Cancel", null, "Camera", "Files");
         if (action == "Camera")
-        {
+        {    // Options for choosing image
             var result = await MediaPicker.CapturePhotoAsync();
             if (result != null)
             {
@@ -63,7 +64,7 @@ public partial class AddJobPage : ContentPage
 
         }
         else if (action == "Files")
-        {
+        { // choose the image
             var result = await FilePicker.PickAsync(new PickOptions
             {
                 PickerTitle = "Pick Image please",
@@ -74,17 +75,17 @@ public partial class AddJobPage : ContentPage
             if (result == null) return;
 
             if(result != null)
-            {
+            {   // finding the source of the image
                 var stream = await result.OpenReadAsync();
                 imageStream = ImageSource.FromStream(() => stream);
                 JobImage.Source = imageStream;
                 try
-                {
+                {   // store the image data in firebase
                     var downloadLink = await new FirebaseStorage(AppConstant.FirebaseStorage).Child("Jobs/" + result.FileName).PutAsync(await result.OpenReadAsync());
                     Console.WriteLine(downloadLink);
                     viewModel.DownloadImage = downloadLink;
 
-                }
+                } // if the exception found
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex);
